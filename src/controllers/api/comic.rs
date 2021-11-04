ccffcbf20678f6b1ec63eca41241ae81d6431197
Alexander Krivács Schrøder <alexschrod@gmail.com>
2021-11-04 08:04:17 +0100

    refactor(comic): Move and add validation to set_<flag> handlers

--- src/controllers/api/comic.rs
+++ src/controllers/api/comic.rs
@@ -2,14 +2,12 @@ use crate::controllers::api::comic::editor_data::fetch_editor_data_for_comic;
 use crate::controllers::api::comic::navigation_data::{
     fetch_all_item_navigation_data, fetch_comic_item_navigation_data,
 };
-use crate::models::{Comic, ComicId, ComicList, Exclusion, Inclusion, ItemColor, ItemType, Token};
-use crate::util::{ensure_is_authorized, NewsUpdater};
+use crate::models::{Comic, ComicId, ComicList, Exclusion, Inclusion, ItemColor, ItemType};
+use crate::util::NewsUpdater;
 use actix_web::{error, web, HttpResponse, Result};
 use actix_web_grants::permissions::{AuthDetails, PermissionsCheck};
 use chrono::{TimeZone, Utc};
-use database::models::{
-    Comic as DatabaseComic, Item as DatabaseItem, LogEntry, News as DatabaseNews,
-};
+use database::models::{Comic as DatabaseComic, Item as DatabaseItem, News as DatabaseNews};
 use database::DbPool;
 use log::info;
 use serde::Deserialize;
@@ -22,6 +20,7 @@ pub(crate) mod navigation_data;
 
 mod add_item;
 mod remove_item;
+mod set_flags;
 mod set_publish_date;
 mod set_tagline;
 mod set_title;
@@ -37,13 +36,15 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
             web::resource("/setpublishdate")
                 .route(web::post().to(set_publish_date::set_publish_date)),
         )
-        .service(web::resource("/setguest").route(web::post().to(set_guest)))
-        .service(web::resource("/setnoncanon").route(web::post().to(set_non_canon)))
-        .service(web::resource("/setnocast").route(web::post().to(set_no_cast)))
-        .service(web::resource("/setnolocation").route(web::post().to(set_no_location)))
-        .service(web::resource("/setnostoryline").route(web::post().to(set_no_storyline)))
-        .service(web::resource("/setnotitle").route(web::post().to(set_no_title)))
-        .service(web::resource("/setnotagline").route(web::post().to(set_no_tagline)))
+        .service(web::resource("/setguest").route(web::post().to(set_flags::set_guest)))
+        .service(web::resource("/setnoncanon").route(web::post().to(set_flags::set_non_canon)))
+        .service(web::resource("/setnocast").route(web::post().to(set_flags::set_no_cast)))
+        .service(web::resource("/setnolocation").route(web::post().to(set_flags::set_no_location)))
+        .service(
+            web::resource("/setnostoryline").route(web::post().to(set_flags::set_no_storyline)),
+        )
+        .service(web::resource("/setnotitle").route(web::post().to(set_flags::set_no_title)))
+        .service(web::resource("/setnotagline").route(web::post().to(set_flags::set_no_tagline)))
         .service(web::resource("/{comicId}").route(web::get().to(by_id)));
 }
 
@@ -261,194 +262,6 @@ async fn by_id(
     Ok(HttpResponse::Ok().json(comic))
 }
 
-async fn set_guest(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::IsGuestComic).await?;
-
-    Ok(HttpResponse::Ok().body("Guest comic set or updated for comic"))
-}
-
-async fn set_non_canon(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::IsNonCanon).await?;
-
-    Ok(HttpResponse::Ok().body("Non-canon set or updated for comic"))
-}
-
-async fn set_no_cast(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::HasNoCast).await?;
-
-    Ok(HttpResponse::Ok().body("No cast set or updated for comic"))
-}
-
-async fn set_no_location(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::HasNoLocation).await?;
-
-    Ok(HttpResponse::Ok().body("No location set or updated for comic"))
-}
-
-async fn set_no_storyline(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::HasNoStoryline).await?;
-
-    Ok(HttpResponse::Ok().body("No storyline set or updated for comic"))
-}
-
-async fn set_no_title(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::HasNoTitle).await?;
-
-    Ok(HttpResponse::Ok().body("No title set or updated for comic"))
-}
-
-async fn set_no_tagline(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-) -> Result<HttpResponse> {
-    set_flag(pool, request, auth, FlagType::HasNoTagline).await?;
-
-    Ok(HttpResponse::Ok().body("No tagline set or updated for comic"))
-}
-
-#[allow(clippy::too_many_lines)]
-async fn set_flag(
-    pool: web::Data<DbPool>,
-    request: web::Json<SetFlagBody>,
-    auth: AuthDetails,
-    flag: FlagType,
-) -> Result<()> {
-    ensure_is_authorized(&auth, token_permissions::CAN_CHANGE_COMIC_DATA)
-        .map_err(error::ErrorForbidden)?;
-
-    let mut transaction = pool
-        .begin()
-        .await
-        .map_err(error::ErrorInternalServerError)?;
-
-    DatabaseComic::ensure_exists_by_id(&mut *transaction, request.comic_id.into_inner())
-        .await
-        .map_err(error::ErrorInternalServerError)?;
-
-    let (true_value_log_text, false_value_log_text, sql_result) = match flag {
-        FlagType::IsGuestComic => (
-            "to be a guest comic",
-            "to be a Jeph comic",
-            DatabaseComic::update_is_guest_comic_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::IsNonCanon => (
-            "to be non-canon",
-            "to be canon",
-            DatabaseComic::update_is_non_canon_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::HasNoCast => (
-            "to have no cast",
-            "to have cast",
-            DatabaseComic::update_has_no_cast_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::HasNoLocation => (
-            "to have no locations",
-            "to have locations",
-            DatabaseComic::update_has_no_location_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::HasNoStoryline => (
-            "to have no storylines",
-            "to have storylines",
-            DatabaseComic::update_has_no_storyline_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::HasNoTitle => (
-            "to have no title",
-            "to have a title",
-            DatabaseComic::update_has_no_title_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-        FlagType::HasNoTagline => (
-            "to have no tagline",
-            "to have a tagline",
-            DatabaseComic::update_has_no_tagline_by_id(
-                &mut *transaction,
-                request.comic_id.into_inner(),
-                request.flag_value,
-            )
-            .await,
-        ),
-    };
-
-    sql_result.map_err(error::ErrorInternalServerError)?;
-
-    LogEntry::log_action(
-        &mut *transaction,
-        request.token.to_string(),
-        format!(
-            "Set comic #{} {}",
-            request.comic_id,
-            if request.flag_value {
-                true_value_log_text
-            } else {
-                false_value_log_text
-            }
-        ),
-    )
-    .await
-    .map_err(error::ErrorInternalServerError)?;
-
-    transaction
-        .commit()
-        .await
-        .map_err(error::ErrorInternalServerError)?;
-
-    Ok(())
-}
-
 fn transfer_item_data_to_navigation_item(
     navigation_items: &mut Vec<crate::models::ItemNavigationData>,
     items: &mut BTreeMap<u16, DatabaseItem>,
@@ -502,22 +315,4 @@ struct ExcludedQuery {
 struct ByIdQuery {
     exclude: Option<Exclusion>,
     include: Option<Inclusion>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct SetFlagBody {
-    token: Token,
-    comic_id: ComicId,
-    flag_value: bool,
-}
-
-pub enum FlagType {
-    IsGuestComic,
-    IsNonCanon,
-    HasNoCast,
-    HasNoLocation,
-    HasNoStoryline,
-    HasNoTitle,
-    HasNoTagline,
 }
