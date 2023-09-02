3c1b1ab48ba9fb14716ef3c7a00e9b3bc751f720
Alexander Krivács Schrøder <alexschrod@gmail.com>
2023-09-02 08:28:13 +0200

    feat: add add_items endpoint

--- src/controllers/v2/api/comic.rs
+++ src/controllers/v2/api/comic.rs
@@ -12,6 +12,7 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/").route(web::get().to(all::all)))
         .service(web::resource("/excluded").route(web::get().to(all::excluded)))
         .service(web::resource("/additem").route(web::post().to(add_item::add_item)))
+        .service(web::resource("/additems").route(web::post().to(add_item::add_items)))
         .service(web::resource("/removeitem").route(web::post().to(remove_item::remove_item)))
         .service(
             web::resource("/{comicId}")
--- src/controllers/v2/api/comic/add_item.rs
+++ src/controllers/v2/api/comic/add_item.rs
@@ -120,6 +120,76 @@ pub(crate) async fn add_item(
     Ok(HttpResponse::Ok().body("Item added to comic"))
 }
 
+pub(crate) async fn add_items(
+    pool: web::Data<DbPool>,
+    request: web::Json<AddItemsToComicBody>,
+    auth: AuthDetails,
+) -> Result<HttpResponse> {
+    ensure_is_authorized(&auth, token_permissions::CAN_ADD_ITEM_TO_COMIC)
+        .map_err(error::ErrorForbidden)?;
+
+    let request = request.into_inner();
+    ensure_is_valid(&request).map_err(error::ErrorBadRequest)?;
+
+    let mut transaction = pool
+        .begin()
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+    DatabaseComic::ensure_exists_by_id(&mut *transaction, request.comic_id.into_inner())
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+    for item in request.items {
+        let item = DatabaseItem::by_id(&mut *transaction, item.item_id)
+            .await
+            .map_err(error::ErrorInternalServerError)?
+            .ok_or_else(|| {
+                error::ErrorBadRequest(anyhow!("Item #{} does not exist", item.item_id))
+            })?;
+
+        let occurrence_exists = Occurrence::occurrence_by_item_id_and_comic_id(
+            &mut *transaction,
+            item.id,
+            request.comic_id.into_inner(),
+        )
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+        if occurrence_exists {
+            // In this multi-item-add scenario, we'll ignore existing items
+            continue;
+        }
+
+        // TODO: Turn off hasNoCast / hasNoLocation / hasNoStoryline flag if enabled
+
+        Occurrence::create(&mut *transaction, item.id, request.comic_id.into_inner())
+            .await
+            .map_err(error::ErrorInternalServerError)?;
+
+        LogEntry::log_action(
+            &mut *transaction,
+            request.token.to_string(),
+            format!(
+                "Added {} #{} ({}) to comic #{}",
+                item.r#type.as_str(),
+                item.id,
+                item.name,
+                request.comic_id
+            ),
+        )
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    }
+
+    transaction
+        .commit()
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().body("Items added to comic"))
+}
+
 #[derive(Debug, Deserialize, TS)]
 #[serde(rename_all = "camelCase")]
 #[ts(export)]
@@ -128,32 +198,6 @@ pub struct AddItemToComicBody {
     pub comic_id: ComicId,
     #[serde(flatten)]
     pub item: ItemBody,
-}
-
-#[derive(Debug, Deserialize, TS)]
-#[serde(untagged)]
-#[ts(export)]
-#[allow(variant_size_differences)]
-pub enum ItemBody {
-    New(NewItem),
-    Existing(ExistingItem),
-}
-
-#[derive(Debug, Default, Deserialize, TS)]
-#[serde(rename_all = "camelCase")]
-#[ts(export)]
-pub struct ExistingItem {
-    pub new: False,
-    pub item_id: u16,
-}
-
-#[derive(Debug, Deserialize, TS)]
-#[serde(rename_all = "camelCase")]
-#[ts(export)]
-pub struct NewItem {
-    pub new: True,
-    pub new_item_name: String,
-    pub new_item_type: ItemType,
 }
 
 impl Validate for AddItemToComicBody {
@@ -193,4 +237,67 @@ pub enum AddItemToComicBodyInvalidity {
     ItemIdInvalid,
     #[display("newItemName value is empty")]
     EmptyNewItemName,
+}
+
+#[derive(Debug, Deserialize, TS)]
+#[serde(rename_all = "camelCase")]
+#[ts(export)]
+pub struct AddItemsToComicBody {
+    pub token: Token,
+    pub comic_id: ComicId,
+    pub items: Vec<ExistingItem>,
+}
+
+impl Validate for AddItemsToComicBody {
+    type Invalidity = AddItemsToComicBodyInvalidity;
+    fn validate(&self) -> semval::ValidationResult<Self::Invalidity> {
+        ValidationContext::new()
+            .validate_with(&self.comic_id, AddItemsToComicBodyInvalidity::ComicId)
+            .invalidate_if(
+                'invalid: {
+                    for item in &self.items {
+                        if item.item_id < 1 {
+                            break 'invalid true;
+                        }
+                    }
+                    false
+                },
+                AddItemsToComicBodyInvalidity::ItemIdInvalid,
+            )
+            .into()
+    }
+}
+
+#[derive(Copy, Clone, Debug, Display, Eq, PartialEq)]
+pub enum AddItemsToComicBodyInvalidity {
+    #[display("{0}")]
+    ComicId(ComicIdInvalidity),
+    #[display("itemIds must all be a value larger than 0")]
+    ItemIdInvalid,
+}
+
+#[derive(Debug, Deserialize, TS)]
+#[serde(untagged)]
+#[ts(export)]
+#[allow(variant_size_differences)]
+pub enum ItemBody {
+    New(NewItem),
+    Existing(ExistingItem),
+}
+
+#[derive(Debug, Default, Deserialize, TS)]
+#[serde(rename_all = "camelCase")]
+#[ts(export)]
+pub struct ExistingItem {
+    pub new: False,
+    pub item_id: u16,
+}
+
+#[derive(Debug, Deserialize, TS)]
+#[serde(rename_all = "camelCase")]
+#[ts(export)]
+pub struct NewItem {
+    pub new: True,
+    pub new_item_name: String,
+    pub new_item_type: ItemType,
 }
